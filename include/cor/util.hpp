03a71cddea894c1216b0360218c16e998fe7ac74
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2012-11-14 17:40:11 +0200

    missing util header
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

---
+++ include/cor/util.hpp
@@ -0,0 +1,84 @@
+#ifndef _COR_UTIL_HPP_
+#define _COR_UTIL_HPP_
+
+#include <cor/error.hpp>
+
+#include <vector>
+#include <cstdio>
+#include <cstdarg>
+#include <stack>
+
+#include <ctime>
+
+namespace cor
+{
+
+template <char Fmt>
+std::string binary_hex(unsigned char const *src, size_t len)
+{
+    static_assert(Fmt == 'X' || Fmt == 'x', "Format should be [xX]");
+    char const *fmt = (Fmt == 'X') ? "%02X" : "%02x";
+    std::string str(len * 2 + 1, '\0');
+    for (size_t i = 0; i < len; ++i)
+        sprintf(&str[i * 2], fmt, src[i]);
+    str.resize(str.size() - 1);
+    return str;
+}
+
+template <char Fmt>
+std::string binary_hex(char const *src, size_t len)
+{
+    return binary_hex<Fmt>(reinterpret_cast<unsigned char const*>(src), len);
+}
+
+template <char Fmt, typename T>
+std::string binary_hex(T const &src)
+{
+    return binary_hex<Fmt>(&src[0], src.size());
+}
+
+static inline unsigned char hex2char(char const *src)
+{
+        unsigned c = 0;
+        int rc = sscanf(src, "%02x", &c);
+        if (!rc)
+            rc = sscanf(src, "%02X", &c);
+        if (rc != 1)
+            throw CError(rc, "Can't match input hex string");
+        return (unsigned char)c;
+}
+
+std::string string_sprintf(size_t max_len, char const *fmt, ...);
+std::vector<unsigned char> hex2bin(char const *src, size_t len);
+
+template <typename DstT>
+std::vector<unsigned char> hex2bin(char const *src, size_t len);
+
+static inline std::vector<unsigned char> hex2bin(std::string const &src)
+{
+    return hex2bin(src.c_str(), src.size());
+}
+
+class Stopwatch
+{
+public:
+    Stopwatch() : begin(std::time(0)) {}
+    std::time_t now() const { return std::time(0) - begin; }
+private:
+    std::time_t begin;
+};
+
+// adding .clear() to std::stack instantiated /w deque
+template <typename T>
+class stack : public std::stack<T, std::deque<T> >
+{
+public:
+    void clear()
+    {
+        this->c.clear();
+    }
+};
+
+} // namespace cor
+
+#endif // _COR_UTIL_HPP_
