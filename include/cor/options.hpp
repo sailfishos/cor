e3d285d7d61cfa8ff3f9824edae1e35f94f942e5
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2012-12-15 14:08:46 +0200

    OptParse: indentation changed to 4 spaces
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- include/cor/options.hpp
+++ include/cor/options.hpp
@@ -39,110 +39,109 @@ template <class StringT>
 class OptParse
 {
 public:
-        typedef std::map<char, char const*> short_opts_type;
-        typedef typename short_opts_type::value_type short_item_type;
-        typedef std::map<StringT, char const*> map_type;
-        typedef typename map_type::value_type item_type;
+    typedef std::map<char, char const*> short_opts_type;
+    typedef typename short_opts_type::value_type short_item_type;
+    typedef std::map<StringT, char const*> map_type;
+    typedef typename map_type::value_type item_type;
 
-        /**
-         * @short_opts list of pairs (short_item_char, item_name)
-         * @long_opts list of pairs (long_item_string, item_name)
-         * @opt_with_params list of item names has params
-         */
-        OptParse(std::initializer_list<short_item_type> short_opts,
-                    std::initializer_list<item_type> long_opts,
-                    std::initializer_list<StringT> opt_with_params)
-                : short_opts_(short_opts),
-                  long_opts_(long_opts),
-                  opt_with_params_(opt_with_params)
-        {}
+    /**
+     * @short_opts list of pairs (short_item_char, item_name)
+     * @long_opts list of pairs (long_item_string, item_name)
+     * @opt_with_params list of item names has params
+     */
+    OptParse(std::initializer_list<short_item_type> short_opts,
+             std::initializer_list<item_type> long_opts,
+             std::initializer_list<StringT> opt_with_params)
+        : short_opts_(short_opts),
+          long_opts_(long_opts),
+          opt_with_params_(opt_with_params)
+    {}
 
-        /** main method to perform options parsing
-         *
-         * @opts map to gather (item_name, value) association
-         * @params container to gather parameters
-         */
-        int parse(int argc, char *argv[],
-                  map_type &opts,
-                  std::vector<char const*> &params)
-        {
-                enum stages {
-                        option,
-                        opt_param
-                };
-                stages stage = option;
-                StringT name;
+    /** main method to perform options parsing
+     *
+     * @opts map to gather (item_name, value) association
+     * @params container to gather parameters
+     */
+    int parse(int argc, char *argv[],
+              map_type &opts,
+              std::vector<char const*> &params)
+    {
+        enum stages {
+            option,
+            opt_param
+        };
+        stages stage = option;
+        StringT name;
 
-                auto parse_short = [&](char const *s, size_t len) {
-                        auto p = short_opts_.find(s[1]);
-                        if (p == short_opts_.end()) {
-                                params.push_back(s);
-                                return;
-                        }
+        auto parse_short = [&](char const *s, size_t len) {
+            auto p = short_opts_.find(s[1]);
+            if (p == short_opts_.end()) {
+                params.push_back(s);
+                return;
+            }
 
-                        name = p->second;
-                        auto p_has = opt_with_params_.find(name);
-                        if (p_has == opt_with_params_.end()) {
-                                opts[name] = nullptr;
-                        } else if (len > 2) {
-                                opts[name] = &s[2];
-                        } else {
-                                stage = opt_param;
-                        }
-                };
+            name = p->second;
+            auto p_has = opt_with_params_.find(name);
+            if (p_has == opt_with_params_.end()) {
+                opts[name] = nullptr;
+            } else if (len > 2) {
+                opts[name] = &s[2];
+            } else {
+                stage = opt_param;
+            }
+        };
 
-                auto parse_long = [&](char const *s, size_t len) {
-                        auto p = long_opts_.find(&s[1]);
-                        if (p == long_opts_.end())
-                                params.push_back(s);
-                        name = p->second;
-                        auto p_has = opt_with_params_.find(name);
-                        if (p_has == opt_with_params_.end())
-                                opts[name] = 0;
-                        else
-                                stage = opt_param;
-                };
+        auto parse_long = [&](char const *s, size_t len) {
+            auto p = long_opts_.find(&s[1]);
+            if (p == long_opts_.end())
+                params.push_back(s);
+            name = p->second;
+            auto p_has = opt_with_params_.find(name);
+            if (p_has == opt_with_params_.end())
+                opts[name] = 0;
+            else
+                stage = opt_param;
+        };
 
-                auto parse_opt_param = [&](char const *s, size_t len) {
-                        if (len >= 1 && s[0] == '-')
-                                throw std::logic_error(s);
-                        opts[name] = s;
-                        stage = option;
-                };
+        auto parse_opt_param = [&](char const *s, size_t len) {
+            if (len >= 1 && s[0] == '-')
+                throw std::logic_error(s);
+            opts[name] = s;
+            stage = option;
+        };
 
-                auto parse_option = [&](char const *s, size_t len) {
-                        if (s[0] == '-') {
-                                if (len > 3 && s[1] == '-')
-                                        parse_long(s, len);
-                                else
-                                        parse_short(s, len);
-                        } else {
-                                params.push_back(s);
-                        }
-                };
+        auto parse_option = [&](char const *s, size_t len) {
+            if (s[0] == '-') {
+                if (len > 3 && s[1] == '-')
+                    parse_long(s, len);
+                else
+                    parse_short(s, len);
+            } else {
+                params.push_back(s);
+            }
+        };
 
-                auto process = [&](char *v) {
-                        size_t len = std::strlen(v);
-                        if (!len)
-                                return;
-                        switch (stage) {
-                        case option:
-                                return parse_option(v, len);
-                        case opt_param:
-                                return parse_opt_param(v, len);
-                        };
-                };
+        auto process = [&](char *v) {
+            size_t len = std::strlen(v);
+            if (!len)
+                return;
+            switch (stage) {
+            case option:
+                return parse_option(v, len);
+            case opt_param:
+                return parse_opt_param(v, len);
+            };
+        };
 
-                std::for_each(argv, argv + argc, process);
-                return 0;
-        }
+        std::for_each(argv, argv + argc, process);
+        return 0;
+    }
 
 private:
-        short_opts_type short_opts_;
-        map_type long_opts_;
-        std::set<StringT> opt_with_params_;
+    short_opts_type short_opts_;
+    map_type long_opts_;
+    std::set<StringT> opt_with_params_;
 };
-
 
 } // cor
 
