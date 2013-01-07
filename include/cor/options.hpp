96e74a4453ad250c33e890a27f61debd8d842d92
Denis Zalevskiy <denis.zalevskiy@jollamobile.com>
2013-01-07 15:14:14 +0200

    compatibility with gcc 4.6
    Signed-off-by: Denis Zalevskiy <denis.zalevskiy@jollamobile.com>

--- include/cor/options.hpp
+++ include/cor/options.hpp
@@ -53,7 +53,7 @@ public:
     OptParse(std::initializer_list<short_item_type> short_opts,
              std::initializer_list<item_type> long_opts,
              std::initializer_list<StringT> opt_with_params,
-             std::initializer_list<StringT> leave_in_params = {})
+             std::initializer_list<StringT> leave_in_params)
         : short_opts_(short_opts),
           long_opts_(long_opts),
           opt_with_params_(opt_with_params),
@@ -178,9 +178,11 @@ public:
                 return;
             switch (stage) {
             case option:
-                return parse_option(v, len);
+                parse_option(v, len);
+                break;
             case opt_param:
-                return parse_opt_param(v, len);
+                parse_opt_param(v, len);
+                break;
             };
         };
 
